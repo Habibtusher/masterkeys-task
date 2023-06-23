799cf94c871c84b7b5c05353a905e6f0be9c5b06
Habibtusher <tusherhrt@gmail.com>
2023-06-23 09:21:51 +0600

    pre prder  section done

--- src/Components/Home/PreOrder.jsx
+++ src/Components/Home/PreOrder.jsx
@@ -1,4 +1,5 @@
 import { AiOutlineArrowRight } from "react-icons/ai";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import preorder1 from "../../assets/preorder1.png";
 const PreOrder = () => {
   return (
@@ -12,17 +13,17 @@ const PreOrder = () => {
         <AiOutlineArrowRight />{" "}
       </div>
       <div className="grid grid-cols-4 mt-56 mb-56">
-        <div></div>
-        <div>
-          <div className="h-56 w-52 bg-[#F5F5F5] rounded-2xl p-2 text-center">
-            <img className="-mt-20 w-full" src={preorder1} alt="" />
-            <p className="text-2xl font-semibold">VERSACE</p>
-            <p className="font-semibold text-lg mt-2">
-              EROS - W - <span className="text-xs font-medium">Women</span>
-            </p>
-            <p className="font-semibold text-lg mt-2">$285.00 USD</p>
+        <div className="mt-8">
+        <div className="flex mt-6 cursor-pointer">
+            <div className="left-arrow h-8 w-8 rounded-full hover:text-[#F4F4F4] text-[#001439] hover:bg-[#001439] bg-[#F4F4F4] mr-3 flex items-center justify-center">
+              {/* <img src={leftarrow} alt="" /> */}
+              <IoIosArrowBack />
+            </div>
+            <div className="right-arrow h-8 w-8 rounded-full hover:text-[#F4F4F4] text-[#001439] hover:bg-[#001439] bg-[#F4F4F4] flex items-center justify-center">
+              {/* <img src={rightarrow} alt=""/>   */}
+              <IoIosArrowForward />
+            </div>
           </div>
-          <button className="btn ml-12 mt-10 btn-neutral  rounded-3xl">Order now</button>
         </div>
         <div>
           <div className="h-56 w-52 bg-[#F5F5F5] rounded-2xl p-2 text-center">
@@ -33,7 +34,9 @@ const PreOrder = () => {
             </p>
             <p className="font-semibold text-lg mt-2">$285.00 USD</p>
           </div>
-          <button className="btn ml-12 mt-10 btn-neutral  rounded-3xl">Order now</button>
+          <button className="btn ml-12 mt-10 btn-neutral  rounded-3xl">
+            Order now
+          </button>
         </div>
         <div>
           <div className="h-56 w-52 bg-[#F5F5F5] rounded-2xl p-2 text-center">
@@ -44,7 +47,22 @@ const PreOrder = () => {
             </p>
             <p className="font-semibold text-lg mt-2">$285.00 USD</p>
           </div>
-          <button className="btn ml-12 mt-10 btn-neutral  rounded-3xl">Order now</button>
+          <button className="btn ml-12 mt-10 btn-neutral  rounded-3xl">
+            Order now
+          </button>
+        </div>
+        <div>
+          <div className="h-56 w-52 bg-[#F5F5F5] rounded-2xl p-2 text-center">
+            <img className="-mt-20 w-full" src={preorder1} alt="" />
+            <p className="text-2xl font-semibold">VERSACE</p>
+            <p className="font-semibold text-lg mt-2">
+              EROS - W - <span className="text-xs font-medium">Women</span>
+            </p>
+            <p className="font-semibold text-lg mt-2">$285.00 USD</p>
+          </div>
+          <button className="btn ml-12 mt-10 btn-neutral  rounded-3xl">
+            Order now
+          </button>
         </div>
       </div>
     </div>
--- src/Components/Home/Product.jsx
+++ src/Components/Home/Product.jsx
@@ -12,8 +12,8 @@ const Product = ({ product }) => {
         <h2 className="card-title text-lg font-semibold">{product?.title}!</h2>
         <div className="flex justify-between items-center">
           <div className="flex gap-8">
-            <p>{product?.price}</p>
-            <p>{product?.price}</p>
+            <p className="text-[#6C6C6C] text-lg font-light ">${product?.price}</p>
+            <p className="text-[#FF4516] text-lg font-light line-through">${(product?.price - (product?.price * product?.discountPercentage)/100)?.toFixed(2)}</p>
           </div>
           <div className="h-12 w-12 bg-[#F0F0F0] rounded-full flex items-center justify-center text-2xl cursor-pointer">
             <AiOutlinePlusCircle />
