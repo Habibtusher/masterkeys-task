8ef11c9a44dfc001db86e0de78080b3eaa3c469c
Habibtusher <tusherhrt@gmail.com>
2023-07-07 09:44:52 +0600

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
 import "./App.css";
-import Footer from "./Components/Home/Footer";
-import Header from "./Components/Home/Header";
 
-import Navbar from "./Components/Home/Navbar";
-import PopularBrands from "./Components/Home/PopularBrands";
-import PreOrder from "./Components/Home/PreOrder";
-import ProductSeller from "./Components/Home/ProductSeller";
-import ShopByCategory from "./Components/Home/ShopByCategory";
-import ShopTrending from "./Components/Home/ShopTrending";
 function App() {
   return (
     <>
-      <Navbar />
-      <Header />
-      <ShopTrending />
-      <ShopByCategory />
-      <PreOrder />
-      <PopularBrands />
-      <ProductSeller />
-      <Footer />
+     hello
     </>
   );
 }
